c4ab1badaafc0a319aa28e9e0c4f07eb4e036b4f
RheeMingyu <stone262@naver.com>
2023-07-18 16:49:54 +0900

    0718 upd

---
+++ jswork/htmlwork/JS/pop.js
@@ -0,0 +1,7 @@
+function popup1(){
+    window.open('../div_img/beauty1.jpg','name','width=400,height=500');
+}
+
+function naver1(){
+    window.open('http://naver.com','naver','width=500,height=500');
+}
